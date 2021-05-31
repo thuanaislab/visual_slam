1498814c79c9655e2e979a5a33f42e610ce80118
thuanaislab <thuan.aislab@gmail.com>
2021-05-31 19:01:23 +0900

    complete read binary sift file

---
+++ Others/visualSfM_Sift_file/Match_modified/test_main.cpp
@@ -0,0 +1,24 @@
+// test.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "FeaturePoints.h"
+
+using namespace std;
+
+int main()
+{   
+    int out;
+    const char* file = "c:\\users\\gr050\\desktop\\test_jpg\\1311868171.063438.sift";
+    const char* file_out = "c:\\users\\gr050\\desktop\\TEST.txt";
+    FeatureData test;
+    out = test.ReadSIFTB(file);
+    if (out){
+        std::cout << "load successful!" << std::endl;
+    }
+    out = test.write_out_data(file_out);
+    //FeatureData::LocationData *location_data;
+
+    return 0;
+
+}
